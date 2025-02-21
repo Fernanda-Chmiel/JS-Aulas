9fc3e5242a88dec6d26cd90dace3d696df3b0cd2
Fernanda-Chmiel <135976905+Fernanda-Chmiel@users.noreply.github.com>
2025-02-21 18:37:28 -0300

    Delete Aula2 directory

--- Aula2/ex2.js
+++
@@ -1,7 +0,0 @@
-const bissexto = 2023;
-
-if (bissexto % 4 == 0) {
-  console.log(`Este ano é bissexto`);
-} else {
-  console.log(`Este ano não é bissexto`);
-}
--- Aula2/ex5.js
+++
@@ -1,9 +0,0 @@
-const distancia = 200;
-const desc1 = distancia * 0.5;
-const desc2 = distancia * 0.45;
-
-if (distancia <= 200) {
-  console.log(`Sua viagem sairá por R$${desc1}`);
-} else {
-  console.log(`Sua viagem sairá por R${desc2}`);
-}
