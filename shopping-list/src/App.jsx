6543a35ffd035844a81a7de0cb08950cdcae56b1
Fernanda-Chmiel <135976905+Fernanda-Chmiel@users.noreply.github.com>
2025-04-03 17:54:28 -0300

    Add files via upload

---
+++ shopping-list/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/header";
+import { Form } from "./components/form";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Form />
+      </div>
+    </>
+  );
+}
