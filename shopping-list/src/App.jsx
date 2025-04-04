9a89dcfd357bca8c072acb4f36e080f7a45b578c
Fernanda-Chmiel <135976905+Fernanda-Chmiel@users.noreply.github.com>
2025-04-04 15:42:04 -0300

    Add files via upload

---
+++ shopping-list/src/App.jsx
@@ -0,0 +1,28 @@
+import { Header } from "./components/header";
+import { Form } from "./components/form";
+import { Product } from "./components/product";
+
+import shoopingList from "../data.json";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Form />
+        <div className="product-list">
+          {shoopingList.lista_de_compras.map(
+            ({ nome, quantidade, tipo }, index) => (
+              <Product
+                key={index}
+                productName={nome}
+                quantity={quantidade}
+                type={tipo}
+              />
+            )
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
